fea7ef01afa5ea7b2780ce68f20d059b99c8772b
CarloBattista <carlitobatti@gmail.com>
2023-04-03 13:58:31 +0200

    generazione bombe

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,22 +1,26 @@
 /*
 
-Consegna:
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare 
+tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, 
+per evitare problemi con l'inizializzazione di git).
 
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa 
+cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
-Ogni cella ha un numero progressivo, da 1 a 100.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella 
+si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti 
+(ovvero quando ha rivelato tutte le celle che non sono bombe).
 
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di 
-azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato 
+su una cella che non era una bomba.
 
-Bonus:
-
+BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 */
 
@@ -44,6 +48,9 @@ function game() {
         let valueSelectValue = selectLevels.options[selectLevels.selectedIndex].value; // Prende gli elementi value dentro la option
         let valueSelectText = selectLevels.options[selectLevels.selectedIndex].text; // Prende gli elementi text dentro la option
         console.log("Hai impostato il livello su: " + valueSelectText)
+
+        let bombs = generateBomb(valueSelectValue, 16);
+        console.log(bombs)
 
         function createDiv(html, classs, text) { // Associo delle richieste alla funzione
             let element = document.createElement(html);
@@ -76,4 +83,19 @@ function myFunction_set(x) {
     x = Math.sqrt(x)
 
     r.style.setProperty('--gridBox', `repeat(${x}, 1fr)`);
+}
+
+// Funzione per generazione bombe
+function generateBomb(difficulty, numberOfBombs){
+    let bombs = []; // Array bombe vuoto
+
+    while (bombs.length < numberOfBombs){
+        let random = Math.floor(Math.random() * difficulty) + 1;
+
+        if(!bombs.includes(random)){
+            bombs.push(random); // Inserisce le bombe dentro l'array
+        }
+    }
+
+    return bombs
 }
